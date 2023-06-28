efb6e1b7b3bc2a6036dd60b9f156cfdc203202a9
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-06-28 23:29:38 +0300

    home-section-header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,24 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (!session) return;
   return (
-    <Layout>Home</Layout>
+    <Layout>
+      <div className="text-text flex justify-between">
+        <h1>
+          Hello, <span className="font-bold">{session?.user?.name}</span>
+        </h1>
+        <div className="flex gap-1 bg-black/60 p-1 pr-2 rounded-lg overflow-hidden">
+          <img
+            src={session?.user?.image}
+            alt={session?.user?.name}
+            className="w-8 h-8 rounded"
+          />
+          <h1 className="">{session?.user?.name}</h1>
+        </div>
+      </div>
+    </Layout>
   );
 }
